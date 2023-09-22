db9329bb817849ff74e55dfbb1dfb7196a3a6792
ismoiljondev <115632460+ismoiljondev@users.noreply.github.com>
2023-09-22 06:08:35 +0500

    file stucture

---
+++ src/constants/http-code.constants.ts
@@ -0,0 +1,3 @@
+export const HTTP_CODE_BAD_REQUEST = 400 as const;
+export const HTTP_CODE_UNAUTHORIZED = 401 as const;
+export const HTTP_CODE_NOT_FOUND = 404 as const;
---
+++ src/types/common.types.d.ts
@@ -0,0 +1,1 @@
+export type ClassNameProps = { className?: string };
