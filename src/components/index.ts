a895b5f5f4bfe9b3d1607f0fa0049b9df9dd0f49
ismoiljondev <115632460+ismoiljondev@users.noreply.github.com>
2023-09-28 21:31:08 +0500

    exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,7 @@
-export * from "@/components/Button/Button";
-export {default as FeatureClientCard} from "@/components/FeatureClientCard/FeatureClientCard";
+export { default as Button } from "@/components/Button/Button";
+export { default as FeatureClientCard } from "@/components/FeatureClientCard/FeatureClientCard";
+export { default as CourseCard } from "@/components/CourseCard/CourseCard";
+export { default as EventsCard } from "@/components/EventCard/EventCard";
+export { default as Grid } from "@/components/Grid/Grid";
+export { default as SwiperCard } from "@/components/Swiper/Swiper";
+export { default as TrainerCard } from "@/components/TrainerCard/TrainerCard";
